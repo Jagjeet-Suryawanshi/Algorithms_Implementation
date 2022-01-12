5673ff657c38af96c0938af09ca291cb8bb44a48
Jagjeet Suryawanshi <96540949+Jagjeet-Suryawanshi@users.noreply.github.com>
2022-01-12 15:00:42 +0530

    Add files via upload

---
+++ Week 2/Checkparenth.c
@@ -0,0 +1,62 @@
+//Program to check the parenthesis
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#define MAX 80
+
+int a = -1;
+char stack[MAX];
+
+void push(char s)
+{
+	stack[++a] = s;
+}
+
+char pop()
+{
+	return stack[a--];
+}
+
+int main()
+{
+	char strin[MAX];
+	char ch;
+	int k = 1, j = 0;
+
+	printf("\nEnter the values as string:-");
+	scanf("%s", strin);
+
+	while (strin[j] != '\0')
+	{
+		if (strin[j] == '(' || strin[j] == '{' || strin[j] == '[')
+			push(strin[j]);
+
+		if (strin[j] == ')' || strin[j] == '}' || strin[j] == ']')
+		{
+			if (a == -1)
+				k = 0;
+			else
+			{
+				ch = pop();
+				if (strin[j] == ')' && ch != '(')
+					k = 0;
+
+				if (strin[j] == '}' && ch != '{')
+					k = 0;
+
+				if (strin[j] == ']' && ch != '[')
+					k = 0;
+			}
+		}
+		j++;
+	}
+	if (a >= 0)
+		k = 0;
+	if (k == 1)
+		printf("\nTRUE");
+	else
+		printf("\nFALSE");
+
+	return 0;
+}
